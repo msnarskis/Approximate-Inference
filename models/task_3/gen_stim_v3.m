545fd780e251756168ba108020f20ac2cf9641fc
msnarsk2 <msnarsk2@bhp0003.bluehive.circ.private>
2021-02-09 18:15:22 -0500

    del old sims, rename relevant sims, add task 3

--- models/task_3/gen_stim_v3.m
+++ models/task_3/gen_stim_v3.m
@@ -1,14 +1,12 @@
 %generate_stimulus_v2([0, 5], 10, 3, [1 1 1; 1 -1 1; -1 -1 1; -1 -1 -1])
-function [s] = gen_stim_by_class(s)
+function [s] = gen_stim_v3(s)
     % Generates (not noisy) Stimulus (e_t) for 
-    % all values of W over n locations
+    % equiv.class values of W over n locations
     % size(stim) = (k: frames, n: location, W: corresponding order)
     
-    % generate W vec
-    s.W = ones(ceil(s.k/2),s.k);
-    for w = 0:floor(s.k/2)
-        s.W(w+1,:) = [ones(1,s.k-w),-1*ones(1,w)];
-    end
+    % generate W vec    
+    s.W = -1*ones(2, s.k);
+    s.W(1,1) = 1;
     
     loc = linspace(s.eps_range(1), s.eps_range(2), s.n); % abs val of locations
     loc_k = repmat(loc, s.k, 1); % repeated
@@ -22,7 +20,7 @@ function [s] = gen_stim_by_class(s)
     end
     
     % note s.corr shaped like model output, not inputs (s.in)
-    s.corr = repmat([0;ones(floor(s.k/2),1)],1,s.n)';
+    s.corr = repmat([1;0],1,s.n)';
     s.eps = loc;
  
 end
